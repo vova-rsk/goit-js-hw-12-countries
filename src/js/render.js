3c42cb2a212ad207c6384f1eb06fa53a4f497b4e
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-07-03 18:55:44 +0300

    refactor

--- src/js/render.js
+++ src/js/render.js
@@ -1,13 +1,19 @@
 import templateCountryInfo from '../templates/country-info.hbs';
 import templateCountriesList from '../templates/country-list.hbs';
 import { outputRef } from '../index';
+import { notice } from './pnotify';
 
 export const renderCountryInfo = data => {
     if (data === '') {
         outputRef.innerHTML = '';
-    } else {
-        outputRef.innerHTML = (data.length === 1)
+        return;
+    }
+    
+    outputRef.innerHTML = (data.length === 1)
         ? templateCountryInfo(...data)
         : templateCountriesList(data);    
+    
+    if (data.length >= 10) {
+        notice();
     }
 };
