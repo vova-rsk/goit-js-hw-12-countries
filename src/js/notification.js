6abd9ff419f36f7de2e1397670dcaa01ca5cef59
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-07-03 23:01:07 +0300

    rename

---
+++ src/js/notification.js
@@ -0,0 +1,22 @@
+import { alert, Stack } from "@pnotify/core";
+
+const stackBottomModal = new Stack({
+  dir1: "right", 
+  firstpos1: 300,
+  push: "bottom", 
+  overlayClose: true,
+  context: document.querySelector(".search-data")
+});
+
+export default function notice(message) {
+  alert({
+    text:`${message}`,
+    width: "320px",
+    type: "error",
+    stack: stackBottomModal,
+    hide: true,
+    delay: 2000,
+    closer: false,
+    sticker: false
+  });
+}
